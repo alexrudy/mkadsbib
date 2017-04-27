e5fcf426f70bef6ff3112a419372bf9d79bb8668
Alex Rudy <alex.rudy@gmail.com>
2017-04-26 21:25:42 -0700

    New features for detecting bibcodes
    - Rudimentary parsing of bibcodes to see if they match ADS format
    - Ability to write out bibcodes in intermediate step

--- mkadsbib.py
+++ mkadsbib.py
@@ -29,22 +29,38 @@ def query(input):
         f.write(ads.ExportQuery(bibcodes).execute())
     click.echo("Exported BibTeX to {0}.".format(f.name))
 
+
 def parse(lines, root="."):
     """Parse a line."""
     for line in lines:
-        citation = re.match(r'\\citation\{([\S]+)\}', line)
+        # ADS Bibcodes always start with the year.
+        citation = re.match(r'\\citation\{([^\s]+)\}', line)
         if citation:
-            yield from citation.group(1).split(",")
+            for citekey in citation.group(1).split(","):
+                if re.match(r"[0-9]{4}[^\s]+", citekey):
+                    yield citekey
         fileinput = re.match(r'\\@?input\{([\S]+)\}', line)
         if fileinput:
-            with open(os.path.join(root, fileinput.group(1)), 'r') as subfile:
+            subfile_name = find_aux_file(os.path.join(root, fileinput.group(1)))
+            with open(subfile_name, 'r') as subfile:
                 yield from parse(subfile, os.path.dirname(subfile.name))
+
+def find_aux_file(filename):
+    """Find aux file, check for latex-out folder."""
+    auxfile = "{0}.aux".format(os.path.splitext(filename)[0])
+    if os.path.exists(auxfile):
+        return auxfile
+    auxout = os.path.join('latex.out', os.path.relpath(auxfile))
+    if os.path.exists(auxout):
+        return auxout
+    raise IOError("Can't locate .aux file for {0}".format(filename))
+    
 
 def get_aux_file(file_obj):
     """Return an .aux file object."""
     if os.path.splitext(file_obj.name)[1] == ".tex":
         file_obj.close()
-        file_obj = open("{0}.aux".format(os.path.splitext(file_obj.name)[0]), 'r')
+        file_obj = open(find_aux_file(file_obj.name), 'r')
     return file_obj
 
 def open_output(output_name, input_name, extension, mode='w'):
@@ -60,20 +76,27 @@ def extract(input, output):
     """Extract citations"""
     with closing(get_aux_file(input)) as aux_file:
         with open_output(output, input.name, extension='.bbq') as f:
-            for citation in parse(input):
+            for citation in sorted(parse(aux_file)):
                 f.write(citation)
-                f.write("\n")]
+                f.write("\n")
     click.echo("Extracted bibcodes from {0} into {1}.".format(input.name, f.name))
 
 @main.command()
 @click.option("-o", "--output", default=None, help="Output file name.")
+@click.option("--bbq/--no-bbq", default=False, help="Output bibcode listing.")
 @click.argument('input', type=click.File(mode='r'))
-def make(input, output):
+def make(input, output, bbq):
     """Make bibliography in one go."""
     with closing(get_aux_file(input)) as aux_file:
-        bibcodes = list(parse(aux_file, os.path.dirname(aux_file.name)))
-        click.echo("Extracted bibcodes from {0}".format(aux_file.name))
+        bibcodes = sorted(set(parse(aux_file, os.path.dirname(aux_file.name))))
+        click.echo("Extracted {0} bibcodes from {1}".format(len(bibcodes), aux_file.name))
+    if bbq:
+        with open_output(output, input.name, extension='.bbq') as f:
+            for bibcode in bibcodes:
+                f.write(bibcode)
+                f.write("\n")
     with open_output(output, input.name, extension=".bib") as f:
+        click.echo("Downloading bibliography to {0}".format(f.name))
         f.write(ads.ExportQuery(bibcodes).execute())
         click.echo("Wrote bibliography to {0}".format(f.name))
 
